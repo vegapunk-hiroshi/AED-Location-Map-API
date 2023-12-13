2c77b5979792210b92ed5b6330fd45a5ccf4276d
John Hiroshi <[vegapunk.hiroshi79@gmail.com]>
2023-12-14 08:10:04 +0900

    Add more origins

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://127.0.0.1:8000 ",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "http://localhost:5173"
 ]
 
 app.add_middleware(
