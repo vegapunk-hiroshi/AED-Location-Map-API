f2f89f3069e32fce06545cbc1b9bef1b0a2853bb
John Hiroshi <[vegapunk.hiroshi79@gmail.com]>
2023-12-18 23:59:01 +0900

    localhost

--- main.py
+++ main.py
@@ -13,10 +13,8 @@ origins = [
     "http://106.172.29.214:80",
     "http://106.172.29.214",
     "https://106.172.29.214",
+    "https://106.172.29.214/",
     "http://localhost",
-    "http://127.0.0.1:8000 ",
-    "http://localhost:5174",
-    "http://localhost:5173"
     "https://d2xmjga6x6xkar.cloudfront.net/"
 ]
 
